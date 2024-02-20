ea96bd7d7c20374b2486e8078249d562153706a3
delivery.api <jadeiltondm@gmail.com>
2024-02-20 20:11:31 -0300

    feat: create controllers

--- src/main/java/lista/celular/lista/controllers/CelularControllers.java
+++ src/main/java/lista/celular/lista/controllers/CelularControllers.java
@@ -1,6 +1,8 @@
 package lista.celular.lista.controllers;
 
 import lista.celular.lista.doMain.Celulares;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +15,14 @@ public class CelularControllers {
 
     private final CelularService celularService;
 
-    public CelularControllers<List<Celulares>> getCelulares(){
 
+    public CelularControllers(CeluaresService celuaresService){
+        this.celularService = celuaresService;
+    }
+    @GetMapping
+    public ResponseEntity<List<Celulares>> getCelulares(){
+        List<Celulares> celulares = this.celularService.getCelularesFromLibriary();
+        return ResponseEntity.ok().body(Celulares);
     }
 
 }
