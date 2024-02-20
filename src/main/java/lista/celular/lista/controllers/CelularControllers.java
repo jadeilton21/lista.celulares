b0c98c75bb725247f6e62926609fa49f1cced424
delivery.api <jadeiltondm@gmail.com>
2024-02-19 22:30:00 -0300

    feat: create class Controller, CelularControllers

--- src/main/java/lista/celular/lista/controllers/CelularControllers.java
+++ src/main/java/lista/celular/lista/controllers/CelularControllers.java
@@ -1,7 +1,10 @@
 package lista.celular.lista.controllers;
 
+import lista.celular.lista.doMain.Celulares;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/celular")
