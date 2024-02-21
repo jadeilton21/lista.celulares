6cfa66a78c7112f729136a86f93205171abaacca
delivery.api <jadeiltondm@gmail.com>
2024-02-21 12:30:33 -0300

    feat: create arguments in Celulares

--- src/main/java/lista/celular/lista/doMain/Celulares.java
+++ src/main/java/lista/celular/lista/doMain/Celulares.java
@@ -1,6 +1,20 @@
 package lista.celular.lista.doMain;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
 public class Celulares {
+
+
+
+    public String marca;
+    public String fabricante;
+    public String cor;
+    public String imagem;
+    public String ano_fabricacao;
+    public String isbn;
 
 
 }
