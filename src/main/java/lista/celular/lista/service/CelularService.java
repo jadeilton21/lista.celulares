a8469eb22445127a7a481af4d165f6d7e082cc6e
delivery.api <jadeiltondm@gmail.com>
2024-02-21 10:45:57 -0300

    feat: create arguments in CelularService

--- src/main/java/lista/celular/lista/service/CelularService.java
+++ src/main/java/lista/celular/lista/service/CelularService.java
@@ -1,6 +1,27 @@
 package lista.celular.lista.service;
 
+
+import lista.celular.lista.doMain.Celulares;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CelularService {
+
+    private final CelularCliente celularCliente;
+
+
+    public CelularService(CelularCliente cliente){
+        this.celularCliente = cliente;
+    }
+
+
+    public List<Celulares> getCelularesFromLibriary(){
+
+        CelularesListResponse response = this.celularCliente.getCelulares();
+        return response.getCelularesList();
+    }
 
 
 
