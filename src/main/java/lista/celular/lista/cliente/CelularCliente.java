6b59a21c7797da345d972ec1d528377b0887b1a5
delivery.api <jadeiltondm@gmail.com>
2024-02-22 08:06:23 -0300

    feat: update CelularCliente

--- src/main/java/lista/celular/lista/cliente/CelularCliente.java
+++ src/main/java/lista/celular/lista/cliente/CelularCliente.java
@@ -1,6 +1,11 @@
 package lista.celular.lista.cliente;
 
 import com.sun.jdi.InterfaceType;
+import lista.celular.lista.reponse.CelularesListResponse;
+import org.springframework.cloud.openfeign.FeignClient;
 
+@FeignClient
 public interface CelularCliente {
+
+    CelularesListResponse getCelulares();
 }
