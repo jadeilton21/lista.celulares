a9f73371a8b3ead2d94deb04f1705487fdfa1507
delivery.api <jadeiltondm@gmail.com>
2024-02-22 22:31:09 -0300

    feat: update

--- src/main/java/lista/celular/lista/cliente/CelularCliente.java
+++ src/main/java/lista/celular/lista/cliente/CelularCliente.java
@@ -3,9 +3,10 @@ package lista.celular.lista.cliente;
 import com.sun.jdi.InterfaceType;
 import lista.celular.lista.reponse.CelularesListResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient
+@FeignClient(name = "celulares-api", url = "https://celulares-exemplos-api.s3.us-east-2.amazonaws.com/celulares.json")
 public interface CelularCliente {
-
+    @GetMapping
     CelularesListResponse getCelulares();
 }
